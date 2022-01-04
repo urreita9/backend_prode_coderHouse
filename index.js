89c9dc30e0c84c7fe5d486c7405f4b4da6129dd3
urreita9 <francisco.urrea9@gmail.com>
2022-01-03 22:22:58 -0300

    Backend Prode 1.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { dbConnection } = require('./database/config');
 require('dotenv').config();
+const { dbConnection } = require('./database/config');
 const cors = require('cors');
 const app = express();
 
