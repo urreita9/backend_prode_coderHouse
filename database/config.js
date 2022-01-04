0a7617d274d6feead7982a0ca20503688df21e54
urreita9 <francisco.urrea9@gmail.com>
2022-01-03 22:20:25 -0300

    Backend Prode 1.0

--- database/config.js
+++ database/config.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
 	try {
 		await mongoose.connect(process.env.DB_CNN, {
-			useCreateIndex: true,
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
-			useFindAndModify: false,
 		});
 
 		console.log('DB online');
